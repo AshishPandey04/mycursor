7957e3bfc707f531e262cd5b96596fb494e55e58
AshishPandey04 <0203ashish2004@gmail.com>
2025-06-13 12:42:08 +0530

    setup myCursor project and test connection .

---
+++ index.js
@@ -0,0 +1,38 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: "AIzaSyDVHLlC_qX7w5krjkzaQkYx8kWyqQTjzB8" });
+function sum(x,y){
+    return x+y;
+}
+const SYSTEM_PROMPT=`
+ You are a helpful AI Assistant who is designed to resolve user query.
+ If you think, user query needs a tool invocation , just tell me the tool name with parameters.
+ OUTPUT FORMAT: 
+ {"tool":"string","text":"string}
+ Available Tools:
+ - sum(x:number , y:number): Returns:number
+`
+
+async function main() {
+  const chat  =  ai.chats.create({
+    model: "gemini-2.0-flash",
+    history:[
+        {
+            role:'model',
+            parts:[{text:SYSTEM_PROMPT}]
+        },
+        {
+            role:'user',
+            parts: [{text:"hey !!"}]
+        }
+    ]
+  });
+
+    const response1 = await chat.sendMessage({
+    message: "what is 5+7",
+  });
+  console.log( response1.text);
+
+}
+
+await main();
